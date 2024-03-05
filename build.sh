ca5c472455499371c154035d732540369f7816ab
oliver-420 <o.daxinger@students.htl-leonding.ac.at>
2024-03-05 11:50:42 +0100

    added build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+mvn -B package
+cp src/main/docker/Dockerfile target/
+
+docker login ghcr.io -u $GITHUB_ACTOR -p $GITHUB_TOKEN
+docker build --tag ghcr.io/$GITHUB_ACTOR/backend:latest ./target
+docker push ghcr.io/$GITHUB_ACTOR/backend:latest
